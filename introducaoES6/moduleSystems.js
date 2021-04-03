2db6d34af5382c5ae3034aaf8fd52397acb32503
Alex Xavier <ajxv20@gmail.com>
2021-04-03 06:15:58 -0300

    Module Systems em JS

---
+++ introducaoES6/moduleSystems.js
@@ -0,0 +1,11 @@
+//Module System - AMD - Asynchronous Module Definition
+define (['./lib/Jquery.js'], function($) {
+  $.ajax
+})
+
+//Module System - Common JS
+var express = require('express')
+
+//Module System - EC6
+import React from 'react'
+export default customModuleSystem
