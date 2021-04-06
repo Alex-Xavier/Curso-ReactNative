c54eee59eed4dbcc279c0d8878fd2b88ee125b04
Alex Xavier <ajxv20@gmail.com>
2021-04-06 06:53:40 -0300

    Tipos de Função

---
+++ introducaoES6/functions.js
@@ -0,0 +1,20 @@
+//Function Types
+
+//Named Function
+function soma1 (x, y) {
+  return x + y
+}
+
+//Anonymous Function
+var soma2 = function (x, y) {
+  return x + y
+}
+
+//ES6 - Arrow Function
+const soma3 = (x, y) => {
+  return x + y
+}
+
+const soma4 = (x, y) => x + y
+
+const array = [1, 2, 3, 4, 5].map(x => x * 100)
