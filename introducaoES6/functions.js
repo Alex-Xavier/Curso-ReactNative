768aaf7d08dd726ad04f5c23f5e6c500e5f7d983
Alex Xavier <ajxv20@gmail.com>
2021-04-06 06:54:30 -0300

    Tipos de função e suas peculiaridades

--- introducaoES6/functions.js
+++ introducaoES6/functions.js
@@ -1,5 +1,8 @@
 //Function Types
 
+/*Algo importante de se destacar é que as Arrow Funtions não contém contexto e portanto
+elas herdam o contexto do escopo onde foram criadas*/
+
 //Named Function
 function soma1 (x, y) {
   return x + y
@@ -15,6 +18,8 @@ const soma3 = (x, y) => {
   return x + y
 }
 
+//ES6 - Simple Arrow Function
 const soma4 = (x, y) => x + y
 
+//ES6 - Array & Map + Simple Arrow Function
 const array = [1, 2, 3, 4, 5].map(x => x * 100)
