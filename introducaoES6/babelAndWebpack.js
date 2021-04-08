1eaddecb5f5f4b1eb31f3226d21244b1077a8a2f
Alex Xavier <ajxv20@gmail.com>
2021-04-08 10:14:04 -0300

    Babel  & Webpack

---
+++ introducaoES6/babelAndWebpack.js
@@ -0,0 +1,12 @@
+/* https://caniuse.com/ - Site para verificar quais browsers suportam
+um código ou uma funcionalidade */
+
+/* Babel - https://babeljs.io/ - É um transpilador de código ES6 para ES5 deixando o código
+compatível com o maior número de navegadores possível - No site do Babel é possível testar
+vários códigos e como eles ficariam transpilados para um código mais compatível */
+
+/* Webpack - Gerenciador de assets - o Webpack condensa as assets em um arquivo
+maior chamado Bundle */
+
+/* Caso queira se aprofundar mais no JavaScript ES5 + ES6 o site do Babel contém uma aba chamada
+Learning muito legal com vários exemplos de estruturas de código */
