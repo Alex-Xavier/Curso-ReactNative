f55021cb7b1266c131fbc4547a6a1d4ae78db2d0
Alex Xavier <ajxv20@gmail.com>
2021-04-09 09:42:54 -0300

    ReactJS - Hello World!!

--- introducaoReactJS/js/index.js
+++ introducaoReactJS/js/index.js
@@ -1,34 +1,7 @@
-class OurHelloWhitClass extends React.Component {
-  render () {
-    return (
-      <h1 className="my-title">Olá!</h1>
-    );
-  }
-}
+// React.createElement(tag, props, childrens)
+const ourTitle = React.createElement('h1', null, 'Hello World!!')
 
-class OurList extends React.Component {
-  render () {
-    return (
-      <ul>
-        <li className="item-01">item 01</li>
-        <li className="item-02">item 02</li>
-        <li className="item-03">item 03</li>
-      </ul>
-    );
-  }
-}
+const container = document.getElementById('app')
 
-class MyWrapper extends React.Component {
-  render () {
-    return (
-      <div className='my-wrapper'>
-        <OurHelloWithClass/>
-        <OurList/>
-      </div>
-    );
-  }
-}
-
-//ReactDOM.render(componente, container)
-const container = document.getElementById("app")
-ReactDOM.render(<MyWrapper/>, container)
+// ReactDOM.render(component, container)
+ReactDOM.render(ourTitle, container)
