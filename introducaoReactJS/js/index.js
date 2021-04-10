11e19ef4b08099c720e5acedc3afa3c2a96d8d43
Alex Xavier <ajxv20@gmail.com>
2021-04-10 06:45:37 -0300

    Iniciando com ReactJS sem JSX

--- introducaoReactJS/js/index.js
+++ introducaoReactJS/js/index.js
@@ -1,7 +1,29 @@
 // React.createElement(tag, props, childrens)
-const ourTitle = React.createElement('h1', null, 'Hello World!!')
+const ourHello = React.createElement('h1', null, 'Hello devs!!')
+
+const ourHelloWithClass = React.createElement(
+  'h1',
+  {className: 'my-title'},
+  'Hello devs!!'
+)
+
+const ourList = React.createElement(
+  'ul',
+  null,
+  [
+    React.createElement('li', {className: 'Dev-01'}, 'Dev-01'),
+    React.createElement('li', {className: 'Dev-02'}, 'Dev-02'),
+    React.createElement('li', {className: 'Dev-03'}, 'Dev-03')
+  ]
+)
+
+const MyWrapper = React.createElement(
+  'div',
+  {className: 'my-wrapper'},
+  [ourHelloWithClass, ourList]
+)
 
 const container = document.getElementById('app')
 
 // ReactDOM.render(component, container)
-ReactDOM.render(ourTitle, container)
+ReactDOM.render(MyWrapper, container)
