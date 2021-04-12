2c0519cd664eb40f4bda2560415d2505bae7cc0b
Alex Xavier <ajxv20@gmail.com>
2021-04-12 10:09:47 -0300

    Introdução ao JSX

--- introducaoReactJS/js/index.js
+++ introducaoReactJS/js/index.js
@@ -1,13 +1,21 @@
-// React.createElement(tag, props, childrens)
+/* React.createElement(tag, props, childrens)
 const ourHello = React.createElement('h1', null, 'Hello devs!!')
 
 const ourHelloWithClass = React.createElement(
   'h1',
   {className: 'my-title'},
   'Hello devs!!'
-)
+)*/
 
-const ourList = React.createElement(
+class OurHelloWithClass extends React.Component {
+  render() {
+    return(
+      <h1 className='my-title'>Hello devs!!</h1>
+    )
+  }
+}
+
+/*const ourList = React.createElement(
   'ul',
   null,
   [
@@ -15,15 +23,38 @@ const ourList = React.createElement(
     React.createElement('li', {className: 'Dev-02'}, 'Dev-02'),
     React.createElement('li', {className: 'Dev-03'}, 'Dev-03')
   ]
-)
+)*/
 
-const MyWrapper = React.createElement(
+class OurList extends Rect.Component {
+  render() {
+    return(
+      <ul>
+        <li className='dev-01'>dev-01</li>
+        <li className='dev-02'>dev-02</li>
+        <li className='dev-03'>dev-03</li>
+      </ul>
+    )
+  }
+}
+
+/*const myWrapper = React.createElement(
   'div',
   {className: 'my-wrapper'},
-  [ourHelloWithClass, ourList]
-)
+  [<OurHelloWithClass />, <OurList />]
+)*/
+
+class MyWrapper extends Recat.Component {
+  render() {
+    return(
+      <div className='my-wrapper'>
+        <OurHelloWithClass />
+        <OurList />
+      </div>
+    )
+  }
+}
 
 const container = document.getElementById('app')
 
 // ReactDOM.render(component, container)
-ReactDOM.render(MyWrapper, container)
+ReactDOM.render(<MyWrapper />, container)
