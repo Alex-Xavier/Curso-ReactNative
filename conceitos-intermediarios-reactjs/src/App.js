633b1cd1ae7860fccc832217bfe51d43e06a5c80
Alex Xavier <ajxv20@gmail.com>
2021-04-20 07:04:31 -0300

    Introdução ao state

--- conceitos-intermediarios-reactjs/src/App.js
+++ conceitos-intermediarios-reactjs/src/App.js
@@ -2,9 +2,27 @@ import React from 'react'
 import './App.css'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      contador: 0,
+      nome: 'Alex Xavier'
+    }
+  }
+
   render() {
+    setTimeout(() => {
+      this.setState({
+        contador: this.state.contador + 1
+      })
+    }, 1000)
+
     return (
-      <h1>Funcionando</h1>
+      <div>
+        <h1>{ this.state.contador }</h1>
+        <h2>{ this.state.nome }</h2>
+      </div>
     )
   }
 }
