bf3baffeedf9b1a4c98a0f31e5cbd35e2b0b1e8f
Alex Xavier <ajxv20@gmail.com>
2021-04-22 10:08:38 -0300

    Criado e renderizado o componente Header na tela

--- Aplicativo_Lista_Contatos/App.js
+++ Aplicativo_Lista_Contatos/App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Header from './src/components/Header'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <View>
+        <Header />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
