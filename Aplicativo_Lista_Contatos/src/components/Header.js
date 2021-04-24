698b4ae76d5ab24336fb388f008bf56b927ec7cb
Alex Xavier <ajxv20@gmail.com>
2021-04-23 23:43:10 -0300

    Estilizado e melhorado o componente Header

--- Aplicativo_Lista_Contatos/src/components/Header.js
+++ Aplicativo_Lista_Contatos/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Header = (props) => (
-  <View>
-    <Text>Este é o nosso Header!</Text>
+  <View style={style.container}>
+    <Text style={style.title}>{ props.title }</Text>
   </View>
 )
 
 export default Header;
+
+style = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    backgroundColor: '#6ca2f7',
+    alignItens: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 50,
+    color: '#FFF'
+  }
+})
