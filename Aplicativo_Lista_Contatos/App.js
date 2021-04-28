bfba74c1b299d0e7e416f3da5e61702eac01e5f4
Alex Xavier <ajxv20@gmail.com>
2021-04-28 10:28:55 -0300

    Buscado os dados da API utilizando o Axios

--- Aplicativo_Lista_Contatos/App.js
+++ Aplicativo_Lista_Contatos/App.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import axios from 'axios'
+
 import Header from './src/components/Header'
 
 export default class App extends React.Component {
   renderList() {
-    const names = [
-      'Pamela',
-      'Patricia',
-      'Escarlete',
-      'Luana'
-    ]
 
-    const textElements = names.map((name) => {
-      return <Text key={name}>{ name }</Text>
-    })
-
-    return textElements
+    // Promises
+    axios
+      .get('https://randomuser.me/api/?nat=br&results=5')
+      .then(response => {
+        const { results } = response.data
+        const name = results.map(people => people.name.first)
+      })
   }
   
   render () {
     return (
       <View>
         <Header title='Pessoas!' />
-        { this.renderList() }
       </View>
     )
   }
