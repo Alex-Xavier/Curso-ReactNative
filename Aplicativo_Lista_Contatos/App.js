916d2f8e35137552ba33fd0b6f19a7ea5f896d59
Alex Xavier <ajxv20@gmail.com>
2021-05-06 10:19:54 -0300

    Instalado e implementado a lib React Navigation

--- Aplicativo_Lista_Contatos/App.js
+++ Aplicativo_Lista_Contatos/App.js
@@ -1,3 +1,14 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
 import { PeopleScreen } from './src/screens'
 
-export default PeopleScreen
+const AppNavigator = createStackNavigator({
+  Main: {
+    screen: PeopleScreen
+  }
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default AppContainer
