d37ee34e246e4d507b66372ccd0ffd8782837189
Alex Xavier <ajxv20@gmail.com>
2021-05-08 08:31:53 -0300

    Iniciado o estilo da tela de Detalhes das Pessoas

--- Aplicativo_Lista_Contatos/src/screens/PeopleDetailScreen.js
+++ Aplicativo_Lista_Contatos/src/screens/PeopleDetailScreen.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 
 export default class PeopleDetailScreen extends React.Component {
   render() {
+    const { people } = this.props.navigation.state.params
+
     return (
-      <View>
+      <View style={styles.container}>
+        <Image
+          source={{ uri: people.picture.large }}
+          style={styles.avatar} />
         <Text>PeopleDetailScreen</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15
+  },
+  avatar: {
+    aspectRatio: 1
+  }
+})
