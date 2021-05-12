679c19a6e37036a4672c7133ad613810b0c79b71
Alex Xavier <ajxv20@gmail.com>
2021-05-12 08:26:03 -0300

    Adicionado componente e variavel boolean loading

--- Aplicativo_Lista_Contatos/src/screens/PeopleScreen.js
+++ Aplicativo_Lista_Contatos/src/screens/PeopleScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 
 import axios from 'axios'
 
@@ -10,25 +10,32 @@ export default class PeopleScreen extends React.Component {
     super(props)
 
     this.state = {
-      peoples: []
+      peoples: [],
+      loading: false
     }
   }
 
   componentDidMount() {
-    // Promises
-    axios
-      .get('https://randomuser.me/api/?nat=br&results=1000')
-      .then(response => {
-        const { results } = response.data
-        this.setState({
-          peoples: results
+    this.setState({ loading: true })
+    
+    setTimeout(() => {
+      axios
+        .get('https://randomuser.me/api/?nat=br&results=1000')
+        .then(response => {
+          const { results } = response.data
+          this.setState({
+            peoples: results,
+            loading: false
+          })
         })
-      })
+    }, 1500)
   }
 
   render () {
     return (
       <View>
+        <ActivityIndicator size='large' color='#6ca2f7' />
+
         <PeopleList
           peoples={this.state.peoples}
           onPressItem={screenParams => this.props.navigation.navigate('PeopleDetail', screenParams)} />
