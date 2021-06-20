f9f97f99384aac3224b85859c4f0b364302d7450
Alex Xavier <ajxv20@gmail.com>
2021-06-20 12:56:37 -0300

    Implementado as Rotas

--- Aplicativo_Lista_Series/App.js
+++ Aplicativo_Lista_Series/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Text } from 'react-native'
 
 import StandardContainer from './src/components/StandardContainer'
+
+import Routes from './src/routes/Routes'
 
 const App = () => {
   return (
     <StandardContainer>
-      <Text>Aplicativo Lista de Séries!</Text>
+      <Routes />
     </StandardContainer>
   )
 }
