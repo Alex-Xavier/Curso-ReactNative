f226be4e884efbddd67eaee65f5efe9b1af1df9f
Alex Xavier <ajxv20@gmail.com>
2021-06-20 15:57:51 -0300

    Adicionado os estilos iniciais do Header

--- Aplicativo_Lista_Series/src/routes/Routes.js
+++ Aplicativo_Lista_Series/src/routes/Routes.js
@@ -9,10 +9,26 @@ const Stack = createStackNavigator()
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator
+        initialRouteName='Login'
+        screenOptions={{
+          title: 'Séries',
+          headerTintColor: '#FFF',
+          headerStyle: {
+            backgroundColor: '#6ca2f7',
+            borderBottomWidth: 1,
+            borderBottomColor: '#C5C5C5'
+          },
+          headerTitleStyle: {
+            color: '#FFF',
+            fontSize: 30
+          }
+        }}
+      >
         <Stack.Screen
           name='Login'
           component={Login}
+          options={{ title: 'Login' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
