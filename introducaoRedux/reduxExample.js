9a259653bb7631bb3c85041e0166df154f2e3e5e
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-07-12 08:59:58 -0300

    Iniciado com State, Action e Dispatch

--- introducaoRedux/reduxExample.js
+++ introducaoRedux/reduxExample.js
@@ -3,3 +3,12 @@ import { createStore } from 'redux'
 const reducer = () => 123
 
 const store = createStore()
+
+const state = store.getState()
+
+const action = {
+  type: 'SET_VALUE',
+  value: 50
+}
+
+store.dispatch(action)
