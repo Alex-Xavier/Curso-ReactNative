b0699f0d300c15ff2a1214ddfe7abd16a97e7fc0
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-07-12 11:03:43 -0300

    Refatorado as Actions

--- introducaoRedux/reduxExample.js
+++ introducaoRedux/reduxExample.js
@@ -1,21 +1,35 @@
 import { createStore } from 'redux'
 
 const SET_VALUE = 'SET_VALUE'
-const action = {
+// Action Creators / Criador de Ações
+const setValue = value => ({
   type: SET_VALUE,
-  value: 50
-}
+  value
+})
+
+const DECREMENT_VALUE = 'DECREMENT_VALUE'
+// Action Creators / Criador de Ações
+const decrementValue = (decrement = 1) => ({
+  type: DECREMENT_VALUE,
+  decrement
+})
 
 const reducer = (state = 0, action) => {
-  if (action.type === SET_VALUE) {
-    return action.value
+  switch (action.type) {
+    case SET_VALUE:
+      return action.value
+    case DECREMENT_VALUE:
+      return state - action.decrement
+    default:
+      return state
   }
-
-  return state
 }
 
-const store = createStore()
+const store = createStore(reducer)
+
+store.dispatch(setValue(50))
+store.dispatch(setValue(100))
+
+store.dispatch(decrementValue(5))
 
 const state = store.getState()
-
-store.dispatch(action)
