8b06fdc7b36dd557ed02521cc567b3ac9ed2c49d
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-07-11 21:00:44 -0300

    Finalizado aula sobre Imutabilidade 1 e 2

---
+++ introducaoRedux/immutability.js
@@ -0,0 +1,50 @@
+/*
+  Pure Functions / Funções Puras - 2
+  -> Immutability / Imutabilidade
+  ->> É algo que não muta / muda o estado de qualquer coisa fora dele mesmo
+*/
+
+let x = 123
+
+
+// Função Inpura / Função Mutável
+const incrementX = () => {
+  return x++
+}
+
+// Função Pura / Função Imutável
+const immutableIncrement = x => {
+  return x + 1
+}
+
+/*--------------------------------------------------------------------------------------------*/
+
+/*
+  Pure Functions / Funções Puras - 3
+  -> Immutability / Imutabilidade
+  ->> É algo que não muta / muda o estado de qualquer coisa fora dele mesmo
+*/
+
+let state = {
+  number: 123,
+  name: 'Alex Xavier'
+}
+
+// Função Inpura / Função Mutável
+const incrementNumber = () => {
+  return state.number++
+}
+
+// Função Inpura / Função Mutável
+const incrementState = stateParameter => {
+  return state.number++
+}
+
+// Função Pura / Função Imutável
+const pureIncrementState = stateParameter => {
+  const newState = Object.assign({}, stateParameter)
+
+  newState.number++
+
+  return newState
+}
