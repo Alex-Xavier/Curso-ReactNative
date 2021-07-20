6e304f55bc77c3e4803845619038d3702f1dec43
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-07-20 10:16:15 -0300

    Testado Dispatch's no Redux

--- praticaRedux/redux.js
+++ praticaRedux/redux.js
@@ -1,21 +1,39 @@
 // Action Types
 const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+// Action Creators
 const increment = () => ({
   type: INCREMENT
 })
-
-const DECREMENT = 'DECREMENT'
 const decrement = () => ({
   type: DECREMENT
 })
 
+// Reducer Functions
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
+      return state + 1
+
     case DECREMENT:
+      return state - 1
+
     default:
       return state
   }
 }
 
 const store = Redux.createStore(reducer)
+
+store.dispatch(increment())
+console.log(store.getState())
+
+store.dispatch(increment())
+console.log(store.getState())
+
+store.dispatch(increment())
+console.log(store.getState())
+
+store.dispatch(descrement())
+console.log(store.getState())
