9985e5c3f5f6c64fbb5c959208dc0e079422274d
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-07-31 11:25:34 -0300

    Finalizado a criação da barra de adição de tarefas

--- Aplicativo_Lista_Tarefas_a_Fazer/src/components/TodoForm.js
+++ Aplicativo_Lista_Tarefas_a_Fazer/src/components/TodoForm.js
@@ -1,18 +1,56 @@
 import React from 'react'
-import { Button, View } from 'react-native'
+import { View, Button, StyleSheet } from 'react-native'
 
 import Input from './Input'
 
-const TodoForm = () => (
-  <View>
-    <View>
-      <Input />
-    </View>
+export default class TodoForm extends React.Component {
+  constructor(props) {
+    super(props)
 
-    <View>
-      <Button />
-    </View>
-  </View>
-)
+    this.state = {
+      text: ''
+    }
+  }
 
-export default TodoForm
+  onChangeText(text) {
+    this.setState({ text })
+  }
+  
+  onPress() {
+    console.log(this.state.text)
+  }
+
+  render() {
+    const { text } = this.state
+
+    return (
+      <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          <Input
+            onChangeText={text => this.onChangeText(text)}
+            value={text}
+          />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title='ADD'
+            onPress={() => this.onPress()}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flexDirection: 'row'
+  },
+  inputContainer: {
+    flex: 4
+  },
+  buttonContainer: {
+    flex: 1
+  }
+})
