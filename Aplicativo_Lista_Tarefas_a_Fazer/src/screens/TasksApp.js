21f8e805dec8e450bd76ac73ce03c907e441d3cc
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-08-08 17:16:47 -0300

    Implementado Redux e a React-Redux

--- Aplicativo_Lista_Tarefas_a_Fazer/src/screens/TasksApp.js
+++ Aplicativo_Lista_Tarefas_a_Fazer/src/screens/TasksApp.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
+import rootReducer from '../reducers'
 import StandardContainer from '../components/StandardContainer'
 import TasksForm from '../components/TasksForm'
+
+const store = createStore(rootReducer)
 
 export default class TasksApp extends React.Component {
   render() {
     return (
-      <StandardContainer>
-        <TasksForm />
-      </StandardContainer>
+      <Provider store={store}>
+        <StandardContainer>
+          <TasksForm />
+        </StandardContainer>
+      </Provider>
     )
   }
 }
