9c8679544eb1bfcadf05fa0af4df4a3e146f0ee7
Alex-Xavier <alex.xavier.ads@gmail.com>
2021-09-26 16:16:55 -0300

    Implementado um ID para cada Task

--- Aplicativo_Lista_Tarefas_a_Fazer/src/reducers/taskListReducer.js
+++ Aplicativo_Lista_Tarefas_a_Fazer/src/reducers/taskListReducer.js
@@ -1,9 +1,12 @@
 import { ADD_TASK } from '../actions'
 
+let nextID = 1
+
 const taskListReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TASK:
       const newTask = {
+        id: nextID++,
         text: action.text
       }
       return [...state, newTask]
